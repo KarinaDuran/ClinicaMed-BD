a1d3f9ea59ff79965e72263679d8dfd0bbc2aa9a
KarinaDuran <karinadmunhos@gmail.com>
2021-12-27 13:41:14 -0300

    Atualizações referentes a data

--- Clinica.java
+++ Clinica.java
@@ -1,7 +1,11 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 
 public class Clinica {
-
+    public Scanner scanner;
     private Connection connection;
 
     public Clinica() {
@@ -12,6 +16,13 @@ public class Clinica {
         }
     }
 
+    public List<Consulta> ListaDeConsulta(){
+        try{
+            List<Consulta> x = new ArrayList<Consulta>(); 
+        }catch(){
+
+        }
+    }
     public void adicionaConsulta(Consulta consulta) {
         try (PreparedStatement stmt = this.connection.prepareStatement(
                 "SELECT * FROM CONSULTA WHERE CRMMEDICO = " + consulta.getCRMMEDICO() + " AND " + consulta.getDtInicio()
@@ -36,11 +47,79 @@ public class Clinica {
                 stmt2.setInt(6, consulta.getPago());
                 stmt2.setInt(7, consulta.getCRMMEDICO());
                 stmt2.setString(8, consulta.getCPFPaciente());
+                stmt2.execute();
+                stmt2.close();
+                System.out.println("Consulta marcada: " + consulta.toString());
             }
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+    }
+
+    public static void options() {
+        System.out.print("========== BEM-VINDO AO SISTEMA CLINICAMED. ==========\n");
+        System.out.println("- Insira o código do comando desejado:\n");
+        System.out.println("(0) Agendar Consulta.\n");
+        System.out.println("(1) Visualizar consultas do dia. \n");
+        System.out.println("(2) Visualizar consultas dos proximos N dias.\n");
+        System.out.println("(3) Visualizar consultas dos ultimos N dias.\n");
+        System.out.println("(4) Alterar dados de uma consulta.\n");
+        System.out.println("(5) Excluir uma consulta.\n");
+        System.out.println("(6) Encerrar.\n");
+        System.out.println("======================================================");
+    }
+
+
+
+    public static void main(String [] args){
+        Clinica c1 = new Clinica();
+
+        
+        String comando;
+        do{
+            options();
+            comando = c1.scanner.nextLine();
+
+            switch(comando){
+
+                case "0":
+                    System.out.println("Insira os dados da consulta. ");
+                    Consulta x = new Consulta();
+                    System.out.println("Id da Consulta: ");
+                    x.setIdConsulta(Integer.parseInt(c1.scanner.nextLine()));
+
+                    System.out.println("Data e horário de início: ");
+                    x.setDtInicio(c1.scanner.nextLine());
+
+                    System.out.println("Data e horário de fim: ");
+                    String id = c1.scanner.nextLine();
+                    x.setIdConsulta(Integer.parseInt(id));
+
+                    System.out.println("Realizada (digite 1 para sim ou 0 para não): ");
+                    x.setRealizada(Integer.parseInt(c1.scanner.nextLine()));
+
+                    System.out.println("Valor Pago: ");
+                    x.setValorPago(Double.parseDouble(c1.scanner.nextLine()));
+
+                    System.out.println("Foi pago (digite 1 para sim ou 0 para não): ");
+                    x.setPago(Integer.parseInt(c1.scanner.nextLine()));
+
+                    System.out.println("CRM do médico: ");
+                    x.setCRMMedico(Integer.parseInt(c1.scanner.nextLine()));
+
+                    System.out.println("CPF do paciente: ");
+                    x.setCPFPaciente(c1.scanner.nextLine());
+
+                    c1.adicionaConsulta(x);
+                    break; 
+                case "1":
+
+            }
+
+        }while(!comando.equals("5"));
+
 
     }
 }
--- Consulta.java
+++ Consulta.java
@@ -1,80 +1,85 @@
 import java.sql.Date;
 import java.time.LocalDateTime;
 
-public class Consulta{
+public class Consulta {
 
-private int IdConsulta;
-private LocalDateTime DtInicio;
-private LocalDateTime DtFim;
-private int Realizada;
-private double ValorPago;
-private int Pago;
-private int CRMMedico;
-private String CPFPaciente;
+    private int IdConsulta;
+    private String DtInicio;
+    private String DtFim;
+    private int Realizada;
+    private double ValorPago;
+    private int Pago;
+    private int CRMMedico;
+    private String CPFPaciente;
 
-public void setIdConsulta(int idConsulta){
-     this.IdConsulta = idConsulta;
-}
+    public void setIdConsulta(int idConsulta) {
+        this.IdConsulta = idConsulta;
+    }
 
-public int getIdConsulta(){
-    return IdConsulta;
-}
+    public int getIdConsulta() {
+        return IdConsulta;
+    }
 
-public void setDtInicio(LocalDateTime DtInicio){
-    this.DtInicio = DtInicio;
-}
+    public void setDtInicio(String DtInicio) {
+        this.DtInicio = DtInicio;
+    }
 
-public LocalDateTime getDtInicio(){
-   return DtInicio;
-}
+    public String getDtInicio() {
+        return DtInicio;
+    }
 
-public void setDtFim(LocalDateTime DtFim){
-    this.DtFim = DtFim;
-}
+    public void setDtFim(String DtFim) {
+        this.DtFim = DtFim;
+    }
 
-public LocalDateTime getDtFim(){
-   return DtFim;
-}
+    public String getDtFim() {
+        return DtFim;
+    }
 
+    public void setRealizada(int Realizada) {
+        this.Realizada = Realizada;
+    }
 
-public void setRealizada(int Realizada){
-    this.Realizada = Realizada;
-}
+    public int getRealizada() {
+        return Realizada;
+    }
 
-public int getRealizada(){
-   return Realizada;
-}
+    public void setValorPago(double ValorPago) {
+        this.ValorPago = ValorPago;
+    }
 
-public void setValorPago(double ValorPago){
-    this.ValorPago = ValorPago;
-}
+    public double getValorPago() {
+        return ValorPago;
+    }
 
-public double getValorPago(){
-   return ValorPago;
-}
+    public void setPago(int Pago) {
+        this.Pago = Pago;
+    }
 
-public void setPago(int Pago){
-    this.Pago = Pago;
-}
+    public int getPago() {
+        return Pago;
+    }
 
-public int getPago(){
-    return Pago;
-}
+    public void setCRMMedico(int CRMMedico) {
+        this.CRMMedico = CRMMedico;
+    }
 
-public void setCRMMedico(int CRMMedico){
-    this.CRMMedico = CRMMedico;
-}
-public int getCRMMEDICO(){
-    return CRMMedico;
-}
+    public int getCRMMEDICO() {
+        return CRMMedico;
+    }
 
-public void setCPFPaciente(String CPFPaciente){
-    this.CPFPaciente = CPFPaciente;
-}
-public String getCPFPaciente(){
-    return CPFPaciente;
-}
+    public void setCPFPaciente(String CPFPaciente) {
+        this.CPFPaciente = CPFPaciente;
+    }
 
+    public String getCPFPaciente() {
+        return CPFPaciente;
+    }
 
-
+    @Override
+    public String toString() {
+        return "IdConsulta: " + IdConsulta + ",/n Data de início: " + DtInicio + ",/n Data do fim: " + DtFim
+                + ",/n Foi realizada: " + Realizada + ",/n Valor Pago: " + ValorPago + ",/n Pago: " + Pago
+                + ",/n CRM do Médico" + CRMMedico + ",/n CPF do paciente: " + CPFPaciente;
+    }
 }
